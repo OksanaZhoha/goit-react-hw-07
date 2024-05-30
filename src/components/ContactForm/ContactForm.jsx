f20b63f6e92dc1873724c4f91dc717b1c7500bc0
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-05-30 12:24:16 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,9 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { useId } from 'react';
-import css from './ContactForm.module.css';
-import { useDispatch } from 'react-redux';
-import { addContact } from '../../redux/contactsSlice';
-import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useId } from "react";
+import css from "./ContactForm.module.css";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import { addContact } from "../../redux/contactsOps";
 
 export default function ContactForm() {
   const dispatch = useDispatch();
@@ -14,24 +14,24 @@ export default function ContactForm() {
   };
 
   const initialValues = {
-    name: '',
-    number: '',
+    name: "",
+    number: "",
   };
 
   const validation = Yup.object().shape({
     name: Yup.string()
-      .min(3, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
     number: Yup.string()
-      .min(3, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
   });
 
   const nameId = useId();
   const numberId = useId();
-  
+
   return (
     <Formik
       initialValues={initialValues}
